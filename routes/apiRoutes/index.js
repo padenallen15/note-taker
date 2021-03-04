d453e914e9067543920510729a3270da84f898b7
Paden Allen <padenallen15@yahoo.com>
2021-03-03 18:52:06 -0600

    can add new notes to notes.json

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const { addNote, findById } = require('../../lib/notes');
+const { notes } = require('../../data/notes.json');
+
+
+
+// GET /api/notes -- should read the notes.json file and return all saved notes as JSON
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+  
+// returns a specific note
+router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+
+// POST /api/notes -- should receive a new note to save on the request body, add it to the notes.json file, and then return the new note to the client.
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    const note = addNote(req.body, notes);
+    res.json(note);
+});
+
+module.exports = router;
