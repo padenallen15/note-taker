0b30366c99a8e4287b90147e16c8394be7d9b8fb
Paden Allen <padenallen15@yahoo.com>
2021-03-07 12:58:50 -0600

    test delete note 2

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,25 +1,25 @@
 const router = require('express').Router();
 const { addNote, findById } = require('../../lib/notes');
 const notes = require('../../data/notes.json');
+// used to assign random id to note
 const {nanoid} = require('nanoid');
 
-
-// GET /api/notes -- should read the notes.json file and return all saved notes as JSON
+// read the notes.json file and return all saved notes as JSON
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
   
-// returns a specific note
+// deletes a note
 router.get('/notes/:id', (req, res) => {
     const result = findById(req.params.id, notes);
     if (result) {
-      res.json(result);
+      deleteNote(result, notes)
     } else {
       res.send(404);
     }
 });
 
-// POST /api/notes -- should receive a new note to save on the request body, add it to the notes.json file, and then return the new note to the client.
+// receive a new note, add it to the notes.json file, and then return the new note to the client.
 router.post('/notes', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = nanoid();
